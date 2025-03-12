2c87acc4c8a39566ff398a8524bc9ae28492e737
Paul Ochieng <paulokello273@gmail.com>
2025-03-13 00:30:39 +0300

    improved loaders

--- app/(platform)/billing/loading.tsx
+++ app/(platform)/billing/loading.tsx
@@ -1,13 +1,15 @@
 import { TypographyMuted } from "@/components/ui/typography";
-import { Loader2 } from "lucide-react";
+import { Loader } from "lucide-react";
 import React from "react";
 
 export default function Loading() {
 	return (
 		<div className="h-screen flex items-center justify-center">
 			<div className="flex items-center gap-6 flex-col">
-				<Loader2 className="size-16 animate-spin text-muted-foreground" />
-				<TypographyMuted className="animate-bounce">Loading...</TypographyMuted>
+				<Loader className="size-32 animate-spin" />
+				<TypographyMuted className="animate-bounce text-lg text-muted-foreground">
+					Loading...
+				</TypographyMuted>
 			</div>
 		</div>
 	);
